0962b75599215fe293323329c4031efdf741bafc
Attaulla <attaulla@truweight.in>
2020-04-01 13:50:00 +0530

    added line hieght

--- src/components/Corona.vue
+++ src/components/Corona.vue
@@ -27,7 +27,7 @@
         <div class="column">
           <b-field>
             <div class="box notification is-warning has-text-centered">
-              <p class="title">Confirmed</p>
+              <p class="title is-spaced">Confirmed</p>
               <p class="subtitle is-2">{{statewises.confirmed}}</p>
             </div>
           </b-field>
@@ -35,7 +35,7 @@
         <div class="column">
           <b-field>
             <div class="box notification is-danger has-text-centered">
-              <p class="title">Death</p>
+              <p class="title is-spaced">Death</p>
               <p class="subtitle is-2">{{statewises.deaths}}</p>
             </div>
           </b-field>
@@ -43,7 +43,7 @@
         <div class="column">
           <b-field>
             <div class="box notification is-success has-text-centered">
-              <p class="title">Recovered</p>
+              <p class="title is-spaced">Recovered</p>
               <p class="subtitle is-2">{{statewises.recovered}}</p>
             </div>
           </b-field>
