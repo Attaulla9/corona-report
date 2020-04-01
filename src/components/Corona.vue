8971a5a2b12657c089fdc48f7f565c40ca5557aa
Attaulla <attaulla@truweight.in>
2020-04-01 13:48:31 +0530

    font size made bolder

--- src/components/Corona.vue
+++ src/components/Corona.vue
@@ -20,7 +20,7 @@
           <b-field>
             <div class="box notification is-primary has-text-centered">
               <p class="title is-3 is-spaced">Active</p>
-              <p>{{statewises.active}}</p>
+              <p class="subtitle is-2">{{statewises.active}}</p>
             </div>
           </b-field>
         </div>
@@ -28,7 +28,7 @@
           <b-field>
             <div class="box notification is-warning has-text-centered">
               <p class="title">Confirmed</p>
-              <p>{{statewises.confirmed}}</p>
+              <p class="subtitle is-2">{{statewises.confirmed}}</p>
             </div>
           </b-field>
         </div>
@@ -36,7 +36,7 @@
           <b-field>
             <div class="box notification is-danger has-text-centered">
               <p class="title">Death</p>
-              <p>{{statewises.deaths}}</p>
+              <p class="subtitle is-2">{{statewises.deaths}}</p>
             </div>
           </b-field>
         </div>
@@ -44,7 +44,7 @@
           <b-field>
             <div class="box notification is-success has-text-centered">
               <p class="title">Recovered</p>
-              <p>{{statewises.recovered}}</p>
+              <p class="subtitle is-2">{{statewises.recovered}}</p>
             </div>
           </b-field>
         </div>
